d8c5f937758a2897182a977b25eb1e6007d79ca8
Andrew Hoang <hoangaatrox23504@gmail.com>
2024-11-23 10:58:07 +0700

    feat(api): Add Api Physical Inventory

--- src/components/Page/PhysicalInventory/PhysicalInventory.vue
+++ src/components/Page/PhysicalInventory/PhysicalInventory.vue
@@ -1,10 +1,13 @@
 <template>
   <div class="container-fluid box-shadow p-3 mx-auto">
     <div class="table-responsive p-md-3">
-      <button type="button" class="btn btn-primary d-flex align-items-center" @click="addInventory">
+      <!-- <button type="button" class="btn btn-primary d-flex align-items-center" @click="addInventory">
         <span class="material-symbols-outlined me-2">add</span>Thêm tồn kho
-      </button>
-      <table class="table ">
+      </button> -->
+      <div class="d-flex justify-content-end mb-3">
+        <SearchInput v-model="searchQuery" placeholder="Tìm kiếm..." />
+      </div>
+      <table class="table">
         <thead>
           <tr>
             <th class="th-id">ID Tồn kho</th>
@@ -13,34 +16,25 @@
             <th class="th-batch-code">Mã lô</th>
             <th class="th-quantity">Số lượng</th>
             <th class="th-update-date">Ngày cập nhật</th>
-            <th class="th-action text-center">Hành động</th>
+            <!-- <th class="th-action text-center">Hành động</th> -->
           </tr>
         </thead>
         <tbody>
-          <tr v-if="inventoryData.length === 0">
+          <tr v-if="inventoriesStore.inventories.length === 0">
             <td colspan="7" class="text-center">Chưa có tồn kho</td>
           </tr>
-          <tr v-for="(inventory, index) in inventoryData" :key="index">
-            <td class="td-id">
-              <input v-model="inventory.idTonKho" type="text" class="form-control" />
-            </td>
-            <td class="td-id-product">
-              <input v-model="inventory.idSanPham" type="text" class="form-control" />
-            </td>
-            <td class="td-warehouse-code">
-              <input v-model="inventory.tenKho" type="text" class="form-control" />
-            </td>
-            <td class="td-batch-code">
-              <input v-model="inventory.maLo" type="text" class="form-control" />
-            </td>
-            <td class="td-quantity">
-              <input v-model.number="inventory.soLuong" type="number" class="form-control" min="0" />
-            </td>
-            <td class="td-update-date">
-              <VueDatePicker v-model="inventory.ngayCapNhat" :enable-time-picker="false" :teleport="true"
-                :format="format" auto-apply :auto-position="true" placeholder="Chọn ngày cập nhật"></VueDatePicker>
-            </td>
-            <td class="td-action">
+          <tr v-for="(inventory, index) in inventoriesStore.inventories" :key="inventory.sysIdTonKho">
+            <td class="td-id">{{ inventory.sysIdTonKho }}</td>
+            <td class="td-id-product">{{ inventory.tenSanPham }}</td>
+            <td class="td-warehouse-code">{{ inventory.maKho }}</td>
+            <td class="td-batch-code">{{ inventory.maLo }}</td>
+            <td class="td-quantity">{{ inventory.soLuong }}</td>
+            <td class="td-update-date">{{ new Date(inventory.ngayCapNhat).toLocaleString('en-GB', {
+              day: '2-digit',
+              month: '2-digit', year: 'numeric', hour: '2-digit', minute: '2-digit', second: '2-digit'
+            }).replace(/\/$/,
+              '').replace(/:\d{2}$/, '') }}</td>
+            <!-- <td class="td-action">
               <div class="d-flex align-items-center justify-content-center">
                 <button type="button" class="btn btn-primary d-flex justify-content-center align-items-center me-2"
                   @click="removeInventory(index)" style="padding: 10px">
@@ -51,7 +45,7 @@
                   <span class="material-symbols-outlined">delete_sweep</span>
                 </button>
               </div>
-            </td>
+            </td> -->
           </tr>
         </tbody>
       </table>
@@ -60,46 +54,22 @@
 </template>
 
 <script setup>
-import { ref, reactive } from 'vue';
-import VueDatePicker from "@vuepic/vue-datepicker";
+import { onMounted } from 'vue';
+import SearchInput from "@/components/Common/Search/SearchInput.vue";
+import { useInventoriesStore } from "@/store/inventoriesStore.js";
 
-const format = (date) => {
-  const day = date.getDate();
-  const month = date.getMonth() + 1;
-  const year = date.getFullYear();
-  return `${day}/${month}/${year}`;
-};
+const inventoriesStore = useInventoriesStore();
 
-const inventoryData = reactive([
-  { idTonKho: 1, idSanPham: 101, tenKho: 'K001', maLo: 'L001', soLuong: 50, ngayCapNhat: new Date() },
-  { idTonKho: 2, idSanPham: 102, tenKho: 'K002', maLo: 'L002', soLuong: 30, ngayCapNhat: new Date() },
-]);
-
-const addInventory = () => {
-  inventoryData.push({
-    idTonKho: inventoryData.length + 1,
-    idSanPham: '',
-    tenKho: '',
-    maLo: '',
-    soLuong: 0,
-    ngayCapNhat: new Date()
-  });
-};
-
-const removeInventory = (index) => {
-  inventoryData.splice(index, 1);
-};
+onMounted(() => {
+  inventoriesStore.getInventories();
+});
 </script>
 
 <style scoped>
 .container-fluid {
-  max-width: 1400px;
+  max-width: 100%;
   border: 1px solid var(--border-main-color);
   border-radius: 1rem;
-}
-
-.btn-danger {
-  padding: 10px;
 }
 
 .th-id,
